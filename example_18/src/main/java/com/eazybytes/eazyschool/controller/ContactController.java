8db922b30c0700841c8fc9ec4551509a8b101007
Sachin Tripathi <sachintripathi0077@gmail.com>
2024-06-18 15:10:10 +0530

    Added WebConfig class for static responses for about and courses. Segregated header and footer from home.html . Added about,courses and contact pages. Added Contact's service and controller along with model.

---
+++ example_18/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -0,0 +1,46 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.service.ContactService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class ContactController {
+    private final ContactService contactService;
+    private static Logger log = LoggerFactory.getLogger(ContactController.class);
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping("/contact")
+    public String displayContactPage() {
+        return "contact.html";
+    }
+
+//    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
+//        log.info("Name: " + name);
+//        log.info("Mobile Number: " + mobileNum);
+//        log.info("Email Address: " + email);
+//        log.info("Subject: " + subject);
+//        log.info("Message: " + message);
+//        return new ModelAndView("redirect:/contact");
+//    }
+
+    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
+    }
+}
+
