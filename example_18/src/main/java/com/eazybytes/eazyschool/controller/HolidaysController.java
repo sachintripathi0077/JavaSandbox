16dc71a0728c818b515913f1fbde828da4b148ae
Sachin Tripathi <sachintripathi0077@gmail.com>
2024-06-22 13:30:40 +0530

    Added h2 db support for holidays page.

--- example_18/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
+++ example_18/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
@@ -1,6 +1,8 @@
 package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Holiday;
+import com.eazybytes.eazyschool.repository.HolidayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidaysController {
+    @Autowired
+    private HolidayRepository holidayRepository;
+
     @GetMapping("/holidays/{display}")
     public String displayHolidays(@PathVariable String display , Model model) {
         if(null != display && display.equals("all")){
@@ -24,16 +29,7 @@ public class HolidaysController {
             model.addAttribute("festival",true);
         }
 
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        List<Holiday> holidays = holidayRepository.findAllHolidays();
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
