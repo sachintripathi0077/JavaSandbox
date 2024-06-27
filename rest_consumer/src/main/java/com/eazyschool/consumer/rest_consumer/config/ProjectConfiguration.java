591a845f081cc3c9e9ed86508e5b348901078e5b
Sachin Tripathi <sachintripathi0077@gmail.com>
2024-06-27 21:53:26 +0530

    Added changes to consume api using: WebClient, RestTemplate

--- rest_consumer/src/main/java/com/eazyschool/consumer/rest_consumer/config/ProjectConfiguration.java
+++ rest_consumer/src/main/java/com/eazyschool/consumer/rest_consumer/config/ProjectConfiguration.java
@@ -1,9 +1,14 @@
 package com.eazyschool.consumer.rest_consumer.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class  ProjectConfiguration {
@@ -11,5 +16,18 @@ public class  ProjectConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthenticationInterceptor(){
         return new BasicAuthRequestInterceptor("admin@gmail.com","admin");
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.basicAuthentication("admin@gmail.com","admin").build();
+    }
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder()
+                .filter(ExchangeFilterFunctions.basicAuthentication("admin@gmail.com","admin"))
+                .build();
     }
 }
--- rest_consumer/src/main/java/com/eazyschool/consumer/rest_consumer/controller/ContactController.java
+++ rest_consumer/src/main/java/com/eazyschool/consumer/rest_consumer/controller/ContactController.java
@@ -1,11 +1,14 @@
 package com.eazyschool.consumer.rest_consumer.controller;
 
 import com.eazyschool.consumer.rest_consumer.model.Contact;
+import com.eazyschool.consumer.rest_consumer.model.Response;
 import com.eazyschool.consumer.rest_consumer.proxy.ContactProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -14,8 +17,38 @@ public class ContactController {
     @Autowired
     ContactProxy contactProxy;
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    WebClient webClient;
+
     @GetMapping("/getMessages")
     public List<Contact> getMessages(@RequestParam("status") String status){
         return contactProxy.getMessagesByStatus(status);
+    }
+
+    @PostMapping("/saveMessage")
+    public ResponseEntity<Response> saveMessage(@RequestBody Contact contact){
+        String uri = "http://localhost:8080/api/contact/saveMessage";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("invocationFrom","RestTemplate");
+
+        HttpEntity<Contact> httpEntity = new HttpEntity<>(contact,httpHeaders);
+
+        ResponseEntity<Response> responseEntity = restTemplate.exchange(uri, HttpMethod.POST,
+                httpEntity, Response.class);
+
+        return responseEntity;
+    }
+
+    @PostMapping("/saveMessageW")
+    public Mono<Response> saveMessageW(@RequestBody Contact contact){
+        String uri = "http://localhost:8080/api/contact/saveMessage";
+        return webClient.post().uri(uri)
+                .header("invocationFrom","WebClient")
+                .body(Mono.just(contact), Contact.class)
+                .retrieve()
+                .bodyToMono(Response.class);
     }
 }
