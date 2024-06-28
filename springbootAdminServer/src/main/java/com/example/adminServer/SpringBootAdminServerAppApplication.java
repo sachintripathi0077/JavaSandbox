d9c0bc7f8825a6881d076993c90ad23a8dda1d24
Sachin Tripathi <sachintripathi0077@gmail.com>
2024-06-28 11:15:54 +0530

    Added project for spring boot admin server.

---
+++ springbootAdminServer/src/main/java/com/example/adminServer/SpringBootAdminServerAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.adminServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerAppApplication.class, args);
+	}
+
+}
