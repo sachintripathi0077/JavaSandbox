5dca6effeb1a421de0b62f8c2b12269dc755fa7e
Sachin Tripathi <sachintripathi0077@gmail.com>
2024-07-04 23:20:35 +0530

    S6: Added support for management actuators [ for refresh ] at runtime.
    This change will allow to refresh the application properties at runtime
    if there is any change in the remote config repository after the
    microservices are deployed.

--- loans/src/main/java/com/dev/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/dev/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,18 @@
 package com.dev.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties("loans")
-public record LoansContactInfoDto(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+@ConfigurationProperties(prefix = "loans")
+@Getter @Setter
+public class LoansContactInfoDto {
+
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
+
 }
