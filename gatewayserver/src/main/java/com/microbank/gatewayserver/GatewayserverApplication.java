ae73b05e00c9083cec757f733cc3f1c3f81e82bf
Sachin Tripathi <sachintripathi0077@gmail.com>
2024-07-09 21:38:47 +0530

    S9: Added route configurations and updated application.yml for gatewayserver
    - Added route configurations in GatewayserverApplication.java for accounts, loans, and cards services using RouteLocator.
    - Updated application.yml in the gatewayserver module to include spring cloud gateway configurations and management endpoint settings.
    - Added Jib Maven plugin configuration in eurekaserver POM file.

--- gatewayserver/src/main/java/com/microbank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microbank/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,36 @@ package com.microbank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator micorBankRounteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route((p)-> p
+						.path("/microbank/accounts/**")
+						.filters(f-> f.rewritePath("/microbank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://ACCOUNTS"))
+
+				.route((p)-> p
+						.path("/microbank/loans/**")
+						.filters(f-> f.rewritePath("/microbank/loans/(?<segment>.*)","/${segment}"))
+						.uri("lb://LOANS"))
+
+				.route((p)-> p
+						.path("/microbank/cards/**")
+						.filters(f-> f.rewritePath("/microbank/cards/(?<segment>.*)","/${segment}"))
+						.uri("lb://CARDS"))
+
+				.build();
 	}
 
 }
