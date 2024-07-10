4de842bb475d0da1a19d7d9f44feac4a6f598350
Sachin Tripathi <sachintripathi0077@gmail.com>
2024-07-10 22:24:19 +0530

    S9: Refactor: Update Docker image tags to S9 across microservices and adjust health checks for consistency

--- gatewayserver/src/main/java/com/microbank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microbank/gatewayserver/GatewayserverApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayserverApplication {
 
@@ -18,17 +20,20 @@ public class GatewayserverApplication {
 		return routeLocatorBuilder.routes()
 				.route((p)-> p
 						.path("/microbank/accounts/**")
-						.filters(f-> f.rewritePath("/microbank/accounts/(?<segment>.*)","/${segment}"))
+						.filters(f-> f.rewritePath("/microbank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://ACCOUNTS"))
 
 				.route((p)-> p
 						.path("/microbank/loans/**")
-						.filters(f-> f.rewritePath("/microbank/loans/(?<segment>.*)","/${segment}"))
+						.filters(f-> f.rewritePath("/microbank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://LOANS"))
 
 				.route((p)-> p
 						.path("/microbank/cards/**")
-						.filters(f-> f.rewritePath("/microbank/cards/(?<segment>.*)","/${segment}"))
+						.filters(f-> f.rewritePath("/microbank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://CARDS"))
 
 				.build();
